d234fd7bc27bf88b86aaddabdd41b209e72f64da
shoukat Malik <shoukatmalik70@gmail.com>
2019-03-04 02:04:38 +0500

    Session # 01 :Trip Crud

---
+++ logisticTracker.Servcies/TripsService.cs
@@ -0,0 +1,39 @@
+﻿using logisticTracker.Data;
+using logisticTracker.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logisticTracker.Servcies
+{
+    public class TripsService
+    {
+        LogisticTrackerDataContext context = new LogisticTrackerDataContext();
+
+        public Trip GetTripByID(int ID)
+        {
+            return context.Trips.Find(ID);
+        }
+        public List<Trip> GetTrips()
+        {
+            return context.Trips.ToList();
+        }
+        public void SaveTrip(Trip trip)
+        {
+            context.Trips.Add(trip);
+            context.SaveChanges();
+        }
+        public void UpdateTrip(Trip trip)
+        {
+            context.Entry(trip).State =System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+        public void DeleteTrip(Trip trip)
+        {
+            context.Entry(trip).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ logisticTracker.Web/Controllers/TripsController.cs
@@ -0,0 +1,56 @@
+﻿using logisticTracker.Entities;
+using logisticTracker.Servcies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace logisticTracker.Web.Controllers
+{
+    public class TripsController : Controller
+    {
+        TripsService TripsService = new TripsService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Trip trip)
+        {
+            TripsService.SaveTrip(trip);
+            return View("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var trip = TripsService.GetTripByID(ID);
+            return View(trip);
+        }
+        [HttpPost]
+        public ActionResult Edit(Trip trip)
+        {
+            TripsService.UpdateTrip(trip);
+            return View("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var trip = TripsService.GetTripByID(ID);
+            return View(trip);
+        }
+        [HttpPost]
+        public ActionResult Delete(Trip trip)
+        {
+            TripsService.DeleteTrip(trip);
+            return View("Index");
+        }
+
+    }
+}
